e8c259543a88c0ad1620f777d34c8d0c11acab12
Hennemuth <wrh@bu.edu>
2026-01-27 14:33:14 -0500

    Exporting AuthToken and TokenAuthConfig

--- bin/index.ts
+++ bin/index.ts
@@ -4,6 +4,7 @@ export { Cache, BasicCache } from '../src/Cache';
 export { Config } from '../src/config/Config';
 export { ConfigManager } from '../src/config/ConfigManager';
 export { ConfigFromEnvironment } from '../src/config/ConfigFromEnvironment';
+export { AuthToken, TokenAuthConfig } from '../src/data-target/AuthToken';
 export { ApiClientForJWT as HuronApiClientForJWT, ApiClientForJWT as HuronApiClient, EndpointConfigForJWT } from '../src/data-target/ApiClientForJWT';
 export { ApiClientForApiKey as HuronApiClientForApiKey, EndpointConfigForApiKey } from '../src/data-source/ApiClientForApiKey';
 export { IApiClient } from '../src/ApiClient';
